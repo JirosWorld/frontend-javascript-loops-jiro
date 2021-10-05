cb0e34bc46070b209f65d381eebdf5c164452262
jolarti <jolarti@gmail.com>
2021-10-05 16:28:17 +0200

    1e loops opdracht

--- loops.js
+++ loops.js
@@ -5,7 +5,9 @@
 // Hoera!
 // Hoera!
 // ==========================================
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!");
+}
 
 
 // ==========================================
@@ -18,7 +20,21 @@
 // klaar!
  // ==========================================
 
+// NOOT JIRO: dit kan op twee manieren:
+// 1e. manier
 
+for (let i = 0; i < 5; i++) {
+    console.log("loop...");
+}
+console.log("klaar!");
+
+//2e. manier
+for (let i = 0; i < 5; i++) {
+    console.log("loop...");
+    if (i >= 4) {
+        console.log("klaar!");
+    }
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -29,6 +45,15 @@
 // 3 is oneven
 // .... etc.
 // 15 is oneven
+
+for (let i = 0; i <= 15; i++) {
+    if (i %2 == 0) {
+        console.log(i, "is even!");
+    }
+    if (i %2 !== 0) {
+        console.log(i, "is oneven.");
+    }
+}
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
@@ -51,3 +76,14 @@
 // >> 9
 // ==========================================
 
+for (let i = 0; i <= 9; i++) {
+    if (i < 3) {
+        console.log(i);
+    }
+    if (i > 2 && i < 7) {
+        console.log(">", i);
+    }
+    if (i > 6) {
+        console.log(">>", i);
+    }
+}
